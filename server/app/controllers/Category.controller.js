c66176d317e31c379a7519073caeb19c696b294f
toidenday113 <toidenday113@gmail.com>
2022-07-26 17:11:03 +0700

    Config page list-category.ejs 26

--- server/app/controllers/Category.controller.js
+++ server/app/controllers/Category.controller.js
@@ -2,7 +2,7 @@ const {getView} = require("../utils/function-utils");
 
 class CategoryController{
     index(req, res){
-        res.render(getView('category.list-category'));
+        res.render(getView('category.index'));
     }
 }
 const categoryController = new CategoryController();
